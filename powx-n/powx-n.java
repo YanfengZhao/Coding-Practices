e43063465fb047202620948c82c5c89fd6f2bccd
YanfengZhao <yanfeng.zhao@utexas.edu>
2015-01-12 16:04:57 -0600

    powx-n.java

--- powx-n/powx-n.java
+++ powx-n/powx-n.java
@@ -12,3 +12,22 @@ public class Solution {
         return n < 0 ? 1.0 / product : product;
     }
 }
+/* recursion
+ public class Solution {
+        public double pow(double x, int m) {
+            double temp=x;
+            if(m==0)
+            return 1;
+            temp=pow(x,m/2);
+            if(m%2==0)
+            return temp*temp;
+            else 
+            {
+            if(m > 0)
+                return x*temp*temp;
+            else
+                return (temp*temp)/x;
+            }
+
+    }
+*/
