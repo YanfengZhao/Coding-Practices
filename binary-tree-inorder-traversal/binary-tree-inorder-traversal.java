7c2f988ddd30132359336942210b8745f7848b30
YanfengZhao <yanfeng.zhao@utexas.edu>
2015-06-24 21:45:39 -0500

    binary-tree-inorder-traversal.java

--- binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
+++ binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
@@ -1,5 +1,5 @@
 /**
- * Definition for binary tree
+ * Definition for a binary tree node.
  * public class TreeNode {
  *     int val;
  *     TreeNode left;
@@ -8,15 +8,33 @@
  * }
  */
 public class Solution {
-    public List<Integer> result = new ArrayList<Integer>();
     public List<Integer> inorderTraversal(TreeNode root) {
-        if(root == null) return result;
-        inorderHelper(root);
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        List<Integer> result = new ArrayList<Integer>();
+        HashSet<TreeNode> visited = new HashSet<TreeNode>();
+        if(root == null){
+            return result;
+        }
+        stack.push(root);
+        
+        while(!stack.empty()){
+            // if already checked this node's left, skip
+            if(!visited.contains(stack.peek())){
+                // keep checking if this node has left node, and push on stack
+                while(stack.peek().left!=null){
+                    visited.add(stack.peek());
+                    visited.add(stack.peek().left);
+                    stack.push(stack.peek().left);
+                }
+            }
+            // pop the current off the stack and add to result
+            TreeNode curr = stack.pop();
+            result.add(curr.val);
+            // if right node exist, push on stack
+            if(curr.right!=null){
+                stack.push(curr.right);
+            }
+        }
         return result;
-    }
-    public void inorderHelper(TreeNode root){
-        if(root.left!=null) inorderHelper(root.left);
-        result.add(root.val);
-        if(root.right!=null) inorderHelper(root.right);
     }
 }
