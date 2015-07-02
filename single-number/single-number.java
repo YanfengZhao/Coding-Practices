a12bdf3bfc70910e5476889d44e9b29be78ab24e
YanfengZhao <yanfeng.zhao@utexas.edu>
2015-07-01 20:17:45 -0500

    single-number.java

--- single-number/single-number.java
+++ single-number/single-number.java
@@ -1,9 +1,18 @@
+/*
+Given an array of integers, every element appears twice except for one. Find that single one.
+
+Note:
+Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+*/
+
+// use EXCLUSIVE OR (XOR)
+
 public class Solution {
-    public int singleNumber(int[] A) {
-        int result = A[0];
-        for(int i=1;i<A.length;i++){
-            result = A[i]^result;
+    public int singleNumber(int[] nums) {
+        int a = 0;
+        for(int n:nums){
+            a^=n;
         }
-        return result;
+        return a;
     }
 }
