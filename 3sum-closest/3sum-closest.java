09eb9286fb5c95605e7347feaebf17a38d9ea7e4
YanfengZhao <yanfeng.zhao@utexas.edu>
2015-07-06 20:19:43 -0500

    3sum-closest.java

--- 3sum-closest/3sum-closest.java
+++ 3sum-closest/3sum-closest.java
@@ -1,34 +1,42 @@
+/*
+Given an array S of n integers, find three integers in S such that the sum is closest to a given number, target. Return the sum of the three integers. You may assume that each input would have exactly one solution.
+
+    For example, given array S = {-1 2 1 -4}, and target = 1.
+
+    The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+*/
+
+// be careful with what you are returning
+
 public class Solution {
-    public int threeSumClosest(int[] num, int target) {
-        if(num == null) return 0;
-        int sum=0;
-        int ans=0;
-        if(num.length < 3){
-            for(int number:num){
-                sum+=number;
-            }
-            return sum;
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int closest = Integer.MAX_VALUE;
+        int closestSum = Integer.MAX_VALUE;
+        for(int i = 0; i <= nums.length - 3; i++){
+    		int start = i+1;
+    		int end = nums.length-1;
+    		while(start<end){
+    			int sum = nums[i]+nums[start]+nums[end];
+    			if(sum == target){
+    				return target;
+    			}
+    			else if(sum > target){
+    				end--;
+    				if(Math.abs(target-sum)<closest){
+    				    closest = Math.abs(target-sum);
+    				    closestSum = sum; 
+    				}
+    			}
+    			else{
+    				start++;
+    				if(Math.abs(target-sum)<closest){
+    				    closest = Math.abs(target-sum);
+    				    closestSum = sum; 
+    				}
+    			}
+        	}
         }
-        Arrays.sort(num);
-        
-        ans = num[0]+num[1]+num[2];
-        for(int i=0; i<num.length-2; i++) {//left
-            int j = i + 1; //middle
-            int k = num.length - 1; //right
-            while(j<k){
-                sum=num[i]+num[j]+num[k];
-                if (Math.abs(target - ans) > Math.abs(target - sum)) {
-                    ans = sum;
-                    if (ans == target) return ans;
-                }
-                if (sum > target){ 
-                    k--;
-                }
-                else{
-                    j++;
-                }
-            }
-        }
-        return ans;
+        return closestSum;
     }
 }
