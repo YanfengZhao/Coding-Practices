95e02eeea1cecb4eb8ba4eb4995812cbb0dc35dd
YanfengZhao <yanfeng.zhao@utexas.edu>
2015-07-07 15:04:15 -0500

    pascals-triangle-ii.java

---
+++ pascals-triangle-ii/pascals-triangle-ii.java
@@ -0,0 +1,22 @@
+/*
+Given an index k, return the kth row of the Pascal's triangle.
+
+For example, given k = 3,
+Return [1,3,3,1].
+
+Note:
+Could you optimize your algorithm to use only O(k) extra space?
+*/
+
+public class Solution {
+    public List<Integer> getRow(int rowIndex) {
+        List<Integer> res = new ArrayList<Integer>();
+        for(int i = 0;i<rowIndex+1;i++) {
+            res.add(1);
+            for(int j=i-1;j>0;j--) {
+                res.set(j, res.get(j-1)+res.get(j));
+            }
+        }
+        return res;
+    }
+}
