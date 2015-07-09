356b6055e2347ea376d1e3f51f3bc160d07aa253
YanfengZhao <yanfeng.zhao@utexas.edu>
2015-07-09 10:28:40 -0500

    validate-binary-search-tree.java

---
+++ validate-binary-search-tree/validate-binary-search-tree.java
@@ -0,0 +1,44 @@
+/*
+Given a binary tree, determine if it is a valid binary search tree (BST).
+
+Assume a BST is defined as follows:
+
+The left subtree of a node contains only nodes with keys less than the node's key.
+The right subtree of a node contains only nodes with keys greater than the node's key.
+Both the left and right subtrees must also be binary search trees.
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+// Uses in-order traversal.
+// keep a global TreeNode prev to ensure later nodes are greater than prev nodes.
+// check left node see if true, then check current node, lastly check the right node.
+
+public class Solution {
+    TreeNode prev = null;
+    public boolean isValidBST(TreeNode root) {
+        return helper(root);
+    }
+    public boolean helper(TreeNode node){
+        if(node == null){
+            return true;
+        }
+        
+        if(!helper(node.left)){
+            return false;
+        }
+        if(prev!=null && prev.val >= node.val){
+            return false;
+        }
+        prev = node;
+        return helper(node.right);
+    }
+}
