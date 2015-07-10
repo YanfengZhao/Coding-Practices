d436f4ef956ddcc8152f3bc9cfe3576f6392a1a9
YanfengZhao <yanfeng.zhao@utexas.edu>
2015-07-09 22:21:31 -0500

    minimum-depth-of-binary-tree.java

--- minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.java
+++ minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.java
@@ -1,3 +1,9 @@
+/*
+Given a binary tree, find its minimum depth.
+
+The minimum depth is the number of nodes along the shortest path from the root node down to the nearest leaf node.
+*/
+
 /**
  * Definition for binary tree
  * public class TreeNode {
@@ -7,23 +13,43 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+ 
+// bfs, if any node at current level has no left or right child, return counter
+
 public class Solution {
     public int minDepth(TreeNode root) {
-        if (root == null) return 0;
-        ArrayList<TreeNode> currentLevel = new ArrayList<TreeNode>();
-        currentLevel.add(root);
-        int depth = 1;
-        while(true){
-            ArrayList<TreeNode> nextLevel = new ArrayList<TreeNode>();
-            for(TreeNode node:currentLevel){
-                if(node.left!=null) nextLevel.add(node.left);
-                if(node.right!=null) nextLevel.add(node.right);
-                if(node.left == null && node.right == null) {
-                    return depth;
+        int counter = 0;
+        if(root == null){
+            return counter;
+        }
+        LinkedList<TreeNode> currLevel = new LinkedList<TreeNode>();
+        currLevel.add(root);
+        while(!currLevel.isEmpty()){
+            counter++;
+            LinkedList<TreeNode> nextLevel = new LinkedList<TreeNode>();
+            for(TreeNode n: currLevel){
+                if(n.left == null && n.right == null){
+                    return counter;
+                }
+                if(n.left!=null){
+                    nextLevel.add(n.left);
+                }
+                if(n.right!=null){
+                    nextLevel.add(n.right);
                 }
             }
-            depth++;
-            currentLevel = nextLevel;
+            currLevel = nextLevel;
         }
+        return counter;
     }
 }
