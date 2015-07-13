0c0554e3c9cb1adcb3067fa02f47782916a99195
YanfengZhao <yanfeng.zhao@utexas.edu>
2015-07-12 21:05:58 -0500

    sort-colors.java update

--- sort-colors/sort-colors.java
+++ sort-colors/sort-colors.java
@@ -8,11 +8,34 @@ You are not suppose to use the library's sort function for this problem.
 */
 
 // This can be done in one pass.
-// The key is to keep 2 pointers, p1 points to the start of 1, p2 points to start of 2.
+// For Solution 1, the key is to keep 2 pointers, p1 points to the end of 0, p2 points to begining of 2.
+// For solution 2, the key is to keep 2 pointers, p1 points to the start of 1, p2 points to start of 2.
+// Solution 1 is more simple.
 
 public class Solution {
     // red = 0, white = 1, blue = 2.
-	public void sortColors(int[] nums) {
+    public void sortColors(int[] nums) {
+        int p1 = 0; // pointer points to the end of 0
+        int p2 = nums.length-1; // pointer points to the beginning of 2
+        for(int i = 0; i <= p2; i++){
+        	if(nums[i] == 0){
+        		swap(nums,i,p1);
+        		p1++;
+        	}
+        	else if(nums[i] == 2){
+        		swap(nums,i,p2);
+        		p2--;
+        		i--;
+        	}
+        }
+    }
+	public void swap(int[] nums, int i1, int i2){
+		int temp = nums[i1];
+		nums[i1] = nums[i2];
+		nums[i2] = temp;
+	}
+	
+	public void sortColorsS2(int[] nums) {
         int p1 = 0; // pointer points to the beginning of 1
         int p2 = 0; // pointer points to the beginning of 2
         for(int i = 0; i < nums.length; i++){
