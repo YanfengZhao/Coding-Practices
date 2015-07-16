05279fce845c6d096d1d7d5f5ac6a06598aa274d
YanfengZhao <yanfeng.zhao@utexas.edu>
2015-07-16 13:10:44 -0500

    binary-tree-level-order-traversal.java update

--- binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
+++ binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
@@ -1,5 +1,23 @@
+/*
+Given a binary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level).
+
+For example:
+Given binary tree {3,9,20,#,#,15,7},
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its level order traversal as:
+[
+  [3],
+  [9,20],
+  [15,7]
+]
+*/
+
 /**
- * Definition for binary tree
+ * Definition for a binary tree node.
  * public class TreeNode {
  *     int val;
  *     TreeNode left;
@@ -7,28 +25,26 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
+
 public class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
         List<List<Integer>> result = new ArrayList<List<Integer>>();
-        List<TreeNode> level = new ArrayList<TreeNode>();
-        if(root == null) return result;
-        level.add(root);
-        
-        while(true){
-            if(level.isEmpty()) break;
-            
-            List<Integer> currLevel = new ArrayList<Integer>();
-            List<TreeNode> nextLevel = new ArrayList<TreeNode>();
-            for(TreeNode node:level){
-                currLevel.add(node.val);
-                if(node.left != null) nextLevel.add(node.left);
-                if(node.right != null) nextLevel.add(node.right);
-            }
-            
-            result.add(currLevel);
-            level = nextLevel;
+        if(root == null){
+            return result;
         }
-        
+        List<TreeNode> currLevel = new ArrayList<TreeNode>();
+        currLevel.add(root);
+        while(currLevel.size() != 0){
+            List<TreeNode> nextLevel = new ArrayList<TreeNode>();
+            List<Integer> currInts = new ArrayList<Integer>();
+            for(TreeNode n: currLevel){
+                currInts.add(n.val);
+                if(n.left!=null) nextLevel.add(n.left);
+                if(n.right!=null) nextLevel.add(n.right);
+            }
+            result.add(currInts);
+            currLevel = nextLevel;
+        }
         return result;
     }
 }
