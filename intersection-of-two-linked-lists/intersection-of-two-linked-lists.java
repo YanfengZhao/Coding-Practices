220cdac4967e4604c8e257f090aea2047f070af5
YanfengZhao <yanfeng.zhao@utexas.edu>
2015-07-21 17:17:51 -0500

    intersection-of-two-linked-lists.java

--- intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
+++ intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
@@ -1,3 +1,25 @@
+/*
+Write a program to find the node at which the intersection of two singly linked lists begins.
+
+
+For example, the following two linked lists:
+
+A:          a1 → a2
+                   ↘
+                     c1 → c2 → c3
+                   ↗            
+B:     b1 → b2 → b3
+begin to intersect at node c1.
+
+
+Notes:
+
+If the two linked lists have no intersection at all, return null.
+The linked lists must retain their original structure after the function returns.
+You may assume there are no cycles anywhere in the entire linked structure.
+Your code should preferably run in O(n) time and use only O(1) memory.
+*/
+
 /**
  * Definition for singly-linked list.
  * public class ListNode {
@@ -9,6 +31,46 @@
  *     }
  * }
  */
+
+// Traverse both list at the same time. 
+// If a becomes null, set it equal to the head of b.
+// If b becomes null, set it eqaul to the head of a.
+// Eventually a will be equal to b, either that's the intersection or null.
+
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if(headA==null || headB==null){
+            return null;
+        }
+        
+        // keep the original linked list
+        ListNode a = headA;
+        ListNode b = headB;
+        
+        while(a != b){
+            if(a == null){
+                a = headB;
+            }
+            else{
+                a = a.next;
+            }
+            if(b == null){
+                b = headA;
+            }
+            else{
+                b = b.next;
+            }
+        }
+        
+        return a;
+    }
+}
+
+// Another solution.
+// Kowning the tail of the two must be the same.
+// Find the length of both lists.
+// Shift the list so that the tails length match, and start to look for the intersection node by node.
+
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
         int lenHeadA = getLength(headA);
