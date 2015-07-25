fd8ec439d09127caa7d4351e1b2b26a6e8cf0f01
YanfengZhao <yanfeng.zhao@utexas.edu>
2015-07-25 00:33:17 -0500

    factorial-trailing-zeroes.java

--- factorial-trailing-zeroes/factorial-trailing-zeros.java
+++ factorial-trailing-zeroes/factorial-trailing-zeros.java
@@ -1,10 +1,20 @@
+/*
+Given an integer n, return the number of trailing zeroes in n!.
+
+Note: Your solution should be in logarithmic time complexity.
+*/
+
+// Count the number of 5s in n add to counter.
+// Divide n by 5 and repeat counting until n becomes 0.
+
 public class Solution {
     public int trailingZeroes(int n) {
-        int zeros=0;
-        while(n>1){
+        int counter = 0;
+
+        while(n!=0){
+            counter+=n/5;
             n/=5;
-            zeros += n;
         }
-        return zeros;
+        return counter;
     }
 }
