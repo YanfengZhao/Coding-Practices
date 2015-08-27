8e0f2132912dae65bfe54a1ccb1f12c615a5c7c0
YanfengZhao <yanfeng.zhao@utexas.edu>
2015-08-27 12:27:13 -0500

    add-two-numbers.java another solution

--- add-two-numbers/add-two-numbers.java
+++ add-two-numbers/add-two-numbers.java
@@ -52,3 +52,74 @@ public class Solution {
         return dummy.next;
     }
 }
+
+// Solution 2 has more complexed code, but the time efficiency is still O(n).
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(-1);
+        ListNode temp = dummy;
+        int carry = 0;
+        while(l1!=null && l2!=null){
+            int sum = l1.val+l2.val+carry;
+            if(sum>9){
+                sum = sum % 10;
+                carry = 1;
+            }
+            else{
+                carry = 0;
+            }
+            ListNode n = new ListNode(sum);
+            temp.next = n;
+            temp = temp.next;
+            l1 = l1.next;
+            l2 = l2.next;
+        }
+        if(l1!=null){
+            while(l1!=null){
+                int sum = l1.val+carry;
+            if(sum>9){
+                sum = sum % 10;
+                carry = 1;
+            }
+            else{
+                carry = 0;
+            }
+            ListNode n = new ListNode(sum);
+            temp.next = n;
+            temp = temp.next;
+            l1 = l1.next;
+            }
+        }
+        if(l2!=null){
+            while(l2!=null){
+                int sum = l2.val+carry;
+            if(sum>9){
+                sum = sum % 10;
+                carry = 1;
+            }
+            else{
+                carry = 0;
+            }
+            ListNode n = new ListNode(sum);
+            temp.next = n;
+            temp = temp.next;
+            l2 = l2.next;
+            }
+        }
+        if(carry==1){
+            ListNode n = new ListNode(1);
+            temp.next = n;
+            temp = temp.next;
+        }
+        return dummy.next;
+    }
+}
