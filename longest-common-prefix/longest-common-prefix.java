3f1f886cd79d2612b5b37f9786b590ab30d1b4aa
YanfengZhao <yanfeng.zhao@utexas.edu>
2015-08-30 15:28:03 -0500

    longest-common-prefix.java bfs approach

--- longest-common-prefix/longest-common-prefix.java
+++ longest-common-prefix/longest-common-prefix.java
@@ -41,3 +41,29 @@ public class Solution {
         }
     }
 }
+
+// BFS approach
+public class Solution2 {
+    public String longestCommonPrefix(String[] strs) {
+        if(strs.length == 0){
+            return "";
+        }
+        String key = strs[0];
+        StringBuilder sb = new StringBuilder();
+        boolean fail = false;
+        for(int j = 0; j < key.length(); j++){
+            for(int i = 1; i < strs.length; i++){
+                if(j>strs[i].length()-1 || strs[i].charAt(j) != key.charAt(j)){
+                    fail = true;
+                }
+            }
+            if(fail == false){
+                sb.append(key.charAt(j));
+            }
+            else{
+                break;
+            }
+        }
+        return sb.toString();
+    }
+}
