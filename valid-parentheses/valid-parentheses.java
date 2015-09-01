870ccb8458305c2f072c3425c092ce878f048e0f
YanfengZhao <yanfeng.zhao@utexas.edu>
2015-09-01 11:49:49 -0500

    valid-parentheses.java more concise version

--- valid-parentheses/valid-parentheses.java
+++ valid-parentheses/valid-parentheses.java
@@ -4,6 +4,38 @@ Given a string containing just the characters '(', ')', '{', '}', '[' and ']', d
 The brackets must close in the correct order, "()" and "()[]{}" are all valid but "(]" and "([)]" are not.
 */
 
+// Solution 1 is more concise
+
+public class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<Character>();
+        
+        for(int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            if(c == '(' || c == '[' || c == '{'){
+                stack.push(c);
+            }
+            else if(stack.isEmpty()){
+                    return false;
+            }
+            else if(c == ')' && stack.peek() == '('){
+                    stack.pop();
+            }
+            else if(c == ']' && stack.peek() == '['){
+                    stack.pop();
+            }
+            else if(c == '}' && stack.peek() == '{'){
+                    stack.pop();
+            }
+            else {
+                return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
+
+// Solution 2
 public class Solution {
     public boolean isValid(String s) {
         Stack<Character> op = new Stack<Character>();
