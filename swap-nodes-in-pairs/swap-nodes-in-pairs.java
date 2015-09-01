472c1b0d66d1b6736164fdd2883295be13e6f178
YanfengZhao <yanfeng.zhao@utexas.edu>
2015-09-01 16:22:51 -0500

    swap-nodes-in-pairs.java recursive solution

--- swap-nodes-in-pairs/swap-nodes-in-pairs.java
+++ swap-nodes-in-pairs/swap-nodes-in-pairs.java
@@ -19,6 +19,25 @@ Your algorithm should use only constant space. You may not modify the values in 
  * }
  */
 
+// Recursion. Deal with 2 nodes at a time.
+// If head or head.next is null, return head since no swap will be performed.
+// Otherwise swap the two nodes and return the first node.
+
+public class Solution {
+    public ListNode swapPairs(ListNode head) {
+        if(head == null || head.next == null){
+            return head;
+        }
+        
+        ListNode last = head.next;
+        head.next = swapPairs(last.next);
+        last.next = head;
+        return last;
+    }
+}
+
+// Solution 2, iteratively.
+
 public class Solution {
   public ListNode swapPairs(ListNode head) {
     ListNode start = new ListNode(0); //make head no longer a special case
