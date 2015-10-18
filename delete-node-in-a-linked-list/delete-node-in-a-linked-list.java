d49c6b25de184caecc9d0a026ab518b131f00aaa
YanfengZhao <yanfeng.zhao@utexas.edu>
2015-10-17 23:41:28 -0500

    delete-node-in-a-linked-list.java optimal 4 line solution

--- delete-node-in-a-linked-list/delete-node-in-a-linked-list.java
+++ delete-node-in-a-linked-list/delete-node-in-a-linked-list.java
@@ -12,7 +12,20 @@ Supposed the linked list is 1 -> 2 -> 3 -> 4 and you are given the third node wi
  *     ListNode(int x) { val = x; }
  * }
  */
- 
+
+// Optimal solution.
+// If node null or node.next is null, don't do anything.
+// Else set current node.val = node.next.val, and delete the next node by
+// node.next = node.next.next.
+
+public class Solution {
+    public void deleteNode(ListNode node) {
+        if(node != null && node.next != null) {
+            node.val = node.next.val;
+            node.next = node.next.next;
+        }
+    }
+}
 // shift the content in the next node into the previous node.
 // remember to keep track of the previous node so at the end you can set the prev.next = null.
 
