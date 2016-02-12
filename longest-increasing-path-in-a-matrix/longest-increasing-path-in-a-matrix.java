e2589b8e84b79c6fd3706a243e30da611ee85e68
YanfengZhao <yanfeng.zhao@utexas.edu>
2016-02-12 14:00:09 -0600

    longest-increasing-path-in-a-matrix.java efficient solution added

--- longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.java
+++ longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.java
@@ -24,6 +24,44 @@ Return 4
 The longest increasing path is [3, 4, 5, 6]. Moving diagonally is not allowed.
 */
 
+// Most efficient solution. O(n*m).
+// The key difference between this and my second "almost" brute force solutin is that
+// each call to dfs, it calcuates the max path starting at current location and stores in the cache.
+// Therefore next time any call to this location you simply check in the cache.
+// Any location that has been visited will be > 0. If == 0, then calculate that location.
+
+public class Solution {
+    public int longestIncreasingPath(int[][] matrix) {
+        if(matrix.length == 0 || matrix[0].length==0){
+            return 0;
+        }
+        int max = 0;
+        int[][] cache = new int[matrix.length][matrix[0].length];
+        for(int i = 0; i < matrix.length; i++){
+            for(int j = 0; j < matrix[0].length; j++){
+                max = Math.max(dfs(matrix,cache,i,j,Integer.MIN_VALUE),max);
+            }
+        }
+        return max;
+    }
+    
+    public int dfs(int[][] matrix, int[][] cache, int i, int j, int prev){
+        if(j<0 || i<0 || j>matrix[0].length-1 || i>matrix.length-1 || matrix[i][j] <= prev){
+            return 0;
+        }
+        if(cache[i][j]!=0){
+            return cache[i][j];
+        }
+        int l = dfs(matrix,cache,i-1,j,matrix[i][j]);
+        int r = dfs(matrix,cache,i+1,j,matrix[i][j]);
+        int t = dfs(matrix,cache,i,j+1,matrix[i][j]);
+        int b = dfs(matrix,cache,i,j-1,matrix[i][j]);
+        cache[i][j] = Math.max(Math.max(Math.max(l,r),t),b)+1;
+        
+        return cache[i][j];
+    }
+}
+
 // This second algorithm isn't the most efficient. Time limit exceeded on Leetcode test but gives the right result.
 // For example
 // nums = [
