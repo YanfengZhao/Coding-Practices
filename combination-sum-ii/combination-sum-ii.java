74ba145c9adefdd43c0ef9ed626a36ba79bd0cc1
YanfengZhao <yanfeng.zhao@utexas.edu>
2016-03-22 17:52:57 -0500

    combination-sum-ii.java with no loops and optimized solution

--- combination-sum-ii/combination-sum-ii.java
+++ combination-sum-ii/combination-sum-ii.java
@@ -19,6 +19,7 @@ A solution set is:
 // Only difference is in this case, everytime you call the helper in the helper method, 
 // you need to increment the start index by 1 and pass it as the index parameter.
 // necessary to use set: [1,1] 1 -> [[1] , [1]] but actually we only want one [1].
+// This is only SECOND  best solution. A more optimiazed solution is below.
 
 public class Solution {
     Set<List<Integer>> result = new HashSet<List<Integer>>();
@@ -48,34 +49,34 @@ public class Solution {
     }
 }
 
-// Second solution, without any loops. A little slower than first version due to
-// more recursive calls.
+// Second solution, without any loops. Optimized. Almost twice as fast as
+// the first solution.
+// The basic idea is just choose or not choose the current number.
 
 public class Solution {
+    Set<List<Integer>> result = new HashSet<List<Integer>>();
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        Set<List<Integer>> result = new HashSet<List<Integer>>();
-        if(candidates == null || candidates.length == 0){
-            return new ArrayList<List<Integer>>();
+        if(candidates.length == 0){
+            return new ArrayList<List<Integer>>(result);
         }
         Arrays.sort(candidates);
-        ArrayList<Integer> curList = new ArrayList<Integer>();
-        helper(result,candidates,target,0,curList);
+        ArrayList<Integer> currList = new ArrayList<Integer>();
+        helper(candidates, target, 0, currList);
         return new ArrayList<List<Integer>>(result);
     }
-    public void helper(Set<List<Integer>> result, int[] candidates, int target, int i, ArrayList<Integer> curList){
-        if(i>candidates.length-1 || target < 0){
+    
+    public void helper(int[] candidates, int target, int start, ArrayList<Integer> currList){
+        if(target == 0){
+            result.add(new ArrayList<Integer>(currList));
             return;
         }
-        if(target == candidates[i]){
-            curList.add(candidates[i]);
-            result.add(new ArrayList<Integer>(curList));
-            curList.remove(curList.size()-1);
-            return;
+        
+        for(int i = start; i < candidates.length; i++){
+            if(target - candidates[i] >= 0){
+                currList.add(candidates[i]);
+                helper(candidates, target - candidates[i], i+1, currList);
+                currList.remove(currList.size()-1);
+            }
         }
-       
-        helper(result, candidates, target, i+1, curList);
-        curList.add(candidates[i]);
-        helper(result, candidates, target-candidates[i], i+1, curList);
-        curList.remove(curList.size()-1);
     }
 }
