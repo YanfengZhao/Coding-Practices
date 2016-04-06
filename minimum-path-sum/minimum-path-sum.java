9b6dfc6230695c8447492a3a1bd67b09dc1e61c9
YanfengZhao <yanfeng.zhao@utexas.edu>
2016-04-05 20:07:32 -0500

    minimum-path-sum.java no extra space

--- minimum-path-sum/minimum-path-sum.java
+++ minimum-path-sum/minimum-path-sum.java
@@ -5,26 +5,23 @@ Note: You can only move either down or right at any point in time.
 */
 
 // DP O(n^2)
+// Modify original array without creating new space.
+
 public class Solution {
     public int minPathSum(int[][] grid) {
-        int[][] min = new int[grid.length][grid[0].length];
-        
-        // preprocess the top and left edge
-        min[0][0] = grid[0][0];
+        // preprocess
         for(int i = 1; i < grid.length; i++){
-            min[i][0] = grid[i][0]+min[i-1][0];
+            grid[i][0] = grid[i-1][0]+grid[i][0];
         }
         for(int i = 1; i < grid[0].length; i++){
-            min[0][i] = grid[0][i] + min[0][i-1];
+            grid[0][i] = grid[0][i-1]+grid[0][i];
         }
         
-        // populate rest of the min
         for(int i = 1; i < grid.length; i++){
             for(int j = 1; j < grid[0].length; j++){
-                min[i][j] = Math.min(min[i-1][j], min[i][j-1])+grid[i][j];
+                grid[i][j] = Math.min(grid[i-1][j],grid[i][j-1]) + grid[i][j];
             }
         }
-        
-        return min[grid.length-1][grid[0].length-1];
+        return grid[grid.length-1][grid[0].length-1];
     }
 }
