504c883b0f13404e790c23d43ceebd7717396a5b
YanfengZhao <yanfeng.zhao@utexas.edu>
2016-04-08 16:26:23 -0500

    set-matrix-zeroes.java update format

--- set-matrix-zeroes/set-matrix-zeroes.java
+++ set-matrix-zeroes/set-matrix-zeroes.java
@@ -4,11 +4,11 @@ Given a m x n matrix, if an element is 0, set its entire row and column to 0. Do
 
 public class Solution {
     public void setZeroes(int[][] matrix) {
-		boolean firstRow0s = false;
-		boolean firstColumn0s = false;
+	boolean firstRow0s = false;
+	boolean firstColumn0s = false;
 		
-		// if a 0 appears, set the corresponding top and left to zero.
-		// if on the edge, make sure mark first row all 0s and first column all 0s for later.
+	// if a 0 appears, set the corresponding top and left to zero.
+	// if on the edge, make sure mark first row all 0s and first column all 0s for later.
         for(int i = 0; i < matrix.length; i++){
         	for(int j = 0; j< matrix[0].length; j++){
         		if(matrix[i][j]==0){
