0f88fb840939400dc2f5488bc5033563e04c7f43
YanfengZhao <yanfeng.zhao@utexas.edu>
2016-07-27 22:19:15 -0700

    largest-number.java adding a faster solution without recursion

--- largest-number/largest-number.java
+++ largest-number/largest-number.java
@@ -6,6 +6,71 @@ For example, given [3, 30, 34, 5, 9], the largest formed number is 9534330.
 Note: The result may be very large, so you need to return a string instead of an integer.
 */
 
+// Similar idea to the second solution below, but without recursion and even faster.
+// For the Comparator class, first see if length of 2 strings are the same.
+// If same, compare character to character.
+// If not same, concatenate the strings in 2 different orders and compare characters to character.
+
+public class Solution {
+    public String largestNumber(int[] nums) {
+        if (nums == null || nums.length == 0){
+            return "";
+        }
+        String[] arr = new String[nums.length];
+        for (int i = 0; i < nums.length; i++){
+            arr[i] = nums[i] + "";
+        }
+        Arrays.sort(arr, new Comp());
+        StringBuilder sb = new StringBuilder();
+        for (String s : arr){
+            if(sb.length() == 0 && s.startsWith("0"))
+		        continue;
+            sb.append(s);
+        }
+        String result = sb.toString();
+        if (result.length() == 0) return "0";
+        return result;
+    }
+    
+    public class Comp implements Comparator<String>{
+        public int compare(String s1, String s2){
+            if (s1.length() == s2.length()){
+                for (int i = 0; i < s1.length(); i++){
+                    char s1c = s1.charAt(i);
+                    char s2c = s2.charAt(i);
+                    if (s1c > s2c) return -1;
+                    else if (s1c < s2c) return 1;
+                }
+                return 0;
+            }
+            String s1First = s1 + s2;
+            String s2First = s2 + s1;
+            for (int i = 0; i < s1First.length(); i++){
+                char s1c = s1First.charAt(i);
+                char s2c = s2First.charAt(i);
+                if (s1c > s2c) return -1;
+                else if (s1c < s2c) return 1;
+            }
+            return 0;
+        }
+        // Could also have done this, but for some reason this is a lot slower. But code is cleaner.
+        /*public int compare(String s1, String s2){
+            if (s1.length() != s2.length()){
+                String temp = s1;
+                s1 = s1 + s2;
+                s2 = s2 + temp;
+            }
+            for (int i = 0; i < s1.length(); i++){
+                char s1c = s1.charAt(i);
+                char s2c = s2.charAt(i);
+                if (s1c > s2c) return -1;
+                else if (s1c < s2c) return 1;
+            }
+            return 0; 
+        }*/
+    }
+}
+
 // Store all integers into an array of Strings.
 // Sort the array of Strings with a customized sorter: Arrays.sort(arr, new Comp());
 // Implement the customized sorter's compare() method. 
@@ -36,7 +101,6 @@ public class Solution {
     }
     
     public class Comp implements Comparator<String>{
-        
         public int compare(String s1, String s2){
             int s1Len = s1.length();
             int s2Len = s2.length();
